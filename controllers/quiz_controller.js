9577f27eee820affa873715a91bdd8eeaa29e386
mperal <m.peral@alumnos.upm.es>
2015-05-08 16:18:09 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,26 @@
+var models = require('../models/models.js');
+
 // GET /quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().then(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res){
-	if(req.query.respuesta.toLowerCase() === 'roma'){
-		res.render('quizes/answer', {respuesta: 'correcta'});
-	}else{
-		res.render('quizes/answer', {respuesta: 'incorrecta'});
-	}
+	models.Quiz.findAll().then(function(quiz){
+		if(req.query.respuesta.toLowerCase() === 'roma'){
+			res.render('quizes/answer', {respuesta: 'correcta'});
+		}else{
+			res.render('quizes/answer', {respuesta: 'incorrecta'});
+		}
+	})
 };
 
 // GET /creditos/author
 exports.author = function(req, res){
-	res.render('creditos/author', {nombre: 'Maria Peral'});
+	models.Quiz.findAll().then(function(quiz){
+		res.render('creditos/author', {nombre: 'Maria Peral'});
+	})
 };

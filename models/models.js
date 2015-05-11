4d35143f503822a4cee5dca66210adcfa529d9b7
mperal <m.peral@alumnos.upm.es>
2015-05-11 12:14:21 +0200

    Despliegue quiz10

--- models/models.js
+++ models/models.js
@@ -3,14 +3,14 @@ var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name  = (url[6]||null);
-var user     = (url[2]||null);
-var pwd      = (url[3]||null);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
 var protocol = (url[1]||null);
-var dialect  = (url[1]||null);
-var port     = (url[5]||null);
-var host     = (url[4]||null);
-var storage  = process.env.DATABASE_STORAGE;
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
@@ -36,13 +36,11 @@ sequelize.sync().then(function(){
 	// success(...) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count){
 		if(count===0){ // la tabla se inicializa solo si está vacía
-			Quiz.create({ pregunta: 'Capital de Italia',
-						  respuesta: 'roma'
-						})
-			Quiz.create({ pregunta: 'Capital de Portugal',
-						  respuesta: 'lisboa'
-						})
-			.then(function(){console.log('Base de datos inicializada')});
+			Quiz.bulkCreate(
+				[ {pregunta: 'Capital de Italia', respuesta: 'Roma'},
+				  {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+				]
+			).then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });

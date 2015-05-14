cb5faaa3b14565ce65c8a4e4bd46f98a3e28286b
mperal <m.peral@alumnos.upm.es>
2015-05-14 12:04:53 +0200

    Despliegue quiz12 con validacion

--- models/quiz.js
+++ models/quiz.js
@@ -5,11 +5,11 @@ module.exports = function(sequelize, DataTypes) {
 		'Quiz',
 		{ pregunta: {
 			type: DataTypes.STRING,
-			validate: {notEmpty: {msg "-> Falta Pregunta"}}
+			validate: {notEmpty: {msg: "-> Falta Pregunta"}}
 		},
 		  respuesta: {
 		  	type: DataTypes.STRING,
-		  	validate: {notEmpty: {msg "-> Falta Respuesta"}}
+		  	validate: {notEmpty: {msg: "-> Falta Respuesta"}}
 			}
 		});
 }

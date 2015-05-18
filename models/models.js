d747f776294a9f734780bcca827b2d9c0427d709
mperal <m.peral@alumnos.upm.es>
2015-05-18 12:55:35 +0200

    Despliegue quiz15 con añadir comentarios

--- models/models.js
+++ models/models.js
@@ -31,7 +31,14 @@ var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "qu
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {

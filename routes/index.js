9b81f800403dda919720fd6bf2f752c98a3aa233
mperal <m.peral@alumnos.upm.es>
2015-05-26 11:33:47 +0200

    Despliegue quiz21 con gestión de usuarios

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
 var statisticsController = require('../controllers/statistics_controller');
-
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,11 +15,20 @@ router.get('/author', quizController.author);
 router.get('/quizes/statistics', statisticsController.statistics);
 router.param('quizId',quizController.load);
 router.param('commentId',commentController.load);
+router.param('userId', userController.load); // autoload :userId
 
 // Definición de rutas de sesion
 router.get('/login', sessionController.new); // formulario login
 router.post('/login', sessionController.create); // crear sesión
 router.get('/logout', sessionController.destroy); // destruir sesión
+
+// Definición de rutas de cuenta
+router.get('/user', userController.new); // formulario sign un
+router.post('/user', userController.create); // registrar usuario
+router.get('/user/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit); // editar información de cuenta
+router.put('/user/:userId(\\d+)', sessionController.loginRequired, userController.update); // actualizar información de cuenta
+router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.destroy); // borrar cuenta
+
 
 
 // Definición de rutas de /quizes

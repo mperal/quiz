c11b80f03de9b42074930e978446e8bbba448d95
mperal <m.peral@alumnos.upm.es>
2015-05-26 12:55:40 +0200

    Despliegue quiz23 con subir imágenes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -77,6 +77,9 @@ exports.new = function(req, res) {
 // POST /quizes/create
 exports.create = function(req, res) {
 	req.body.quiz.UserId = req.session.user.id;
+	if(req.files.image){
+		req.body.quiz.image=req.files.image.name;
+	}
 	var quiz = models.Quiz.build( req.body.quiz );
 
 quiz
@@ -87,7 +90,7 @@ quiz
  			res.render('quizes/new', {quiz: quiz, errors: err.errors});
  		} else {
  			quiz // save: guarda en DB campos pregunta y respuesta de quiz
- 			.save({fields: ["pregunta", "respuesta","UserId"]})
+ 			.save({fields: ["pregunta", "respuesta","UserId","image"]})
  			.then( function(){ res.redirect('/quizes')})
  		} // res.redirect: Redirección HTTP a lista de preguntas
  	}
@@ -102,6 +105,9 @@ exports.edit = function(req, res) {
 
 // PUT /quizes/:id
 exports.update = function(req, res) {
+	if(req.files.image){
+		req.body.quiz.image=req.files.image.name;
+	}
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
 	req.quiz
@@ -112,7 +118,7 @@ exports.update = function(req, res) {
  		res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
  	} else {
  		req.quiz // save: guarda campos pregunta y respuesta en DB
- 	.save( {fields: ["pregunta", "respuesta"]})
+ 	.save( {fields: ["pregunta", "respuesta","image"]})
  	.then( function(){ res.redirect('/quizes');});
  	} // Redirección HTTP a lista de preguntas (URL relativo)
  	}

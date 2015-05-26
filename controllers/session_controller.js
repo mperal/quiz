4594215e8fd5422b83b328ed1e1975715e18d6b3
mperal <m.peral@alumnos.upm.es>
2015-05-26 12:30:20 +0200

    Despliegue quiz22 con roles y permisos

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -27,7 +27,7 @@ exports.create = function(req, res) {
 		}
 		// Crear req.session.user y guardar campos id y username
 		// La sesión se define por la existencia de: req.session.user
-		req.session.user = {id:user.id, username:user.username, tiempoUltimoMov:new Date().getTime()};
+		req.session.user = {id:user.id, username:user.username, tiempoUltimoMov:new Date().getTime(),isAdmin:user.isAdmin};
 		res.redirect(req.session.redir.toString());// redirección a path anterior a login
  	});
 };

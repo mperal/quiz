d8ad9f69deb9019ce35c0c74db6c90365097af07
mperal <m.peral@alumnos.upm.es>
2015-05-26 16:09:10 +0200

    Despliegue quiz24 con algunos errores solucionados

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -1,14 +1,36 @@
-var numPreguntas = 0;
-var numComentarios = 0;
-var numMedioComentarios = 0;
-var numPreguntasSinComentarios = 0;
+var models = require('../models/models.js');
+
+
 
 exports.statistics = function(req,res) {
-	res.render('quizes/statistics.ejs', {
+	var numPreguntas=0;
+	var numComentarios=0;
+	var numMedioComentarios=0;
+	var numPreguntasConComentarios=0;
+	var quiz = req.quiz;
+	var i=0;
+	models.Comment.findAll().then(function(comment){
+		numComentarios=comment.length;
+	});
+	models.Quiz.findAll({include: [{model: models.Comment}]}).then(function(quizes){
+		numPreguntas=quizes.length;
+		for(i=0;i<numPreguntas;i++){
+			if(quizes[i].Comments.length)numPreguntasConComentarios++;
+		}
+		// si hay preguntas, se reinicializa numMedioComentarios
+		if(numPreguntas) numMedioComentarios = numComentarios/numPreguntas;
+
+		res.render('quizes/statistics.ejs', {
 		numPreguntas: numPreguntas, 
 		numComentarios:numComentarios,
 		numMedioComentarios:numMedioComentarios, 
-		numPreguntasSinComentarios:numPreguntasSinComentarios,
+		numPreguntasConComentarios:numPreguntasConComentarios,
 		errors: []
+		});
 	});
+
+
+
+	
+	
 };
